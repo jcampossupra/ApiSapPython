7f56a649bd97c5ccefd031d9fad1ac27f3b05817
unknown <firewall2@L-SU-IT-001.supralive.remoto>
2025-02-27 17:00:56 -0500

    actualizacion para herokue

--- app.py
+++ app.py
@@ -11,28 +11,26 @@ port = 30015
 user = 'SUPRALIVE'
 password = 'uGDH6%Yr$K'
 database = 'SUPRALIVE'
-
 # Cadena de conexión a SAP HANA
 connection_string = f'DRIVER={{HDBODBC}};SERVERNODE={address}:{port};UID={user};PWD={password};DATABASE={database}'
 
-# Configuración de logging para manejo de errores
+# para manejo de errores
 logging.basicConfig(level=logging.INFO)
 
 @app.route('/consultar_sap', methods=['GET'])
 def consultar_sap():
     try:
-        # Conexión a la base de datos SAP HANA
+        # Conexión a SAP HANA
         conn = pyodbc.connect(connection_string)
         cursor = conn.cursor()
 
-        # Consulta SQL a SAP HANA
         cursor.execute("""SELECT T0."ItemCode" FROM SBO_EC_TENA12_02.OITM T0 WHERE T0."QryGroup5" = 'Y'""")
         result = cursor.fetchall()
 
         # Crear una lista de resultados
         result_list = []
         for row in result:
-            result_list.append({'ItemCode': row[0]})  # row[0] accede al valor de la columna 'ItemCode'
+            result_list.append({'ItemCode': row[0]})  # accedo al valor de la columna 'ItemCode'
         cursor.close()
         conn.close()
 
@@ -44,4 +42,6 @@ def consultar_sap():
         return jsonify({"error": "Hubo un problema al realizar la consulta. Intenta de nuevo más tarde."}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Puerto Heroku
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto proporcionado por Heroku o 5000 por defecto
+    app.run(host='0.0.0.0', port=port, debug=True)
